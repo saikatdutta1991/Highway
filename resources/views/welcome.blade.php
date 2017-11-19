825224498e75f0452e3525d27066f35326f4092a
saikat <saikatdutta1991@gmail.com>
2017-11-19 23:04:50 +0530

    welcome page background image changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,8 +13,9 @@
         <!-- Styles -->
         <style>
             html, body {
+                background: url('{{url('admin_assets')}}/background.jpg');
                 background-color: #fff;
-                color: #636b6f;
+                color: white;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
@@ -50,7 +51,7 @@
             }
 
             .links > a {
-                color: #636b6f;
+                color: white;
                 padding: 0 25px;
                 font-size: 12px;
                 font-weight: 600;
@@ -62,6 +63,9 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+
+
         </style>
     </head>
     <body>
@@ -79,7 +83,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    HighWay
+                    HighWay Trip
                 </div>
 
                 <div class="links">
