7afb6e38f70d7116feaf114fbc293d77602e33ae
saikat <saikatdutta1991@gmail.com>
2017-11-19 23:07:26 +0530

    admin login page media query  enable

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -2,6 +2,7 @@
 <html >
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Highway Trip Admin LOGIN</title>
     <link rel="stylesheet" href="{{url('admin_assets')}}/css/style.css">
     <style type="text/css">
