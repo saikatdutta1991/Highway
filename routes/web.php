5635e7d69b0336df782424cc42fa3ab1d5bba411
saikat <saikatdutta1991@gmail.com>
2017-12-10 03:12:11 +0530

    web routes changed

--- routes/web.php
+++ routes/web.php
@@ -96,9 +96,6 @@ Route::get('send-sms', function(){
 
 Route::get('send-email', function(){
 
-    
-    
-    $user = App\Models\User::where('email', 'saikatdutta1991@gmail.com')->first();
     return new \App\Mail\WelcomeUser($user);
     app('App\Repositories\Email')->sendNewUserWelcomeEmail($user);
 
