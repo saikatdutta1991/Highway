a7b1ea3e9caed7aaa6b23ed8334bfe1685619866
saikat <saikatdutta1991@gmail.com>
2018-10-25 11:43:37 +0530

    SocketServer mysql disconnect error modified

--- NodeServer/SocketServer.js
+++ NodeServer/SocketServer.js
@@ -50,17 +50,36 @@ if (config.IS_HTTPS) {
 /**
  * initilizing mysql database connection
  */
-var conn = mysql.createConnection(config.mysql);
-conn.connect(function (err) {
+var conn;
 
-	if (err) {
-		console.log('mysql database connection failed', err.message);
-		process.exit(1);
-	}
+function handleDisconnect() {
+	conn = mysql.createConnection(config.mysql); // Recreate the connection, since
+	// the old one cannot be reused.
+
+	conn.connect(function (err) {              // The server is either down
+		if (err) {                                     // or restarting (takes a while sometimes).
+			console.log('error when connecting to db:', err);
+			setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+		}                                     // to avoid a hot loop, and to allow our node script to
+	});                                     // process asynchronous requests in the meantime.
+	// If you're also serving http, display a 503 error.
+	conn.on('error', function (err) {
+		console.log('db error', err);
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+			handleDisconnect();                         // lost due to either server restart, or a
+		} else {                                      // connnection idle timeout (the wait_timeout
+			throw err;                                  // server variable configures this)
+		}
+	});
 
 	console.log("mysql database connected");
 
-});
+}
+
+handleDisconnect();
+
+
+
 
 
 var helper = require('./Helper')(conn);
