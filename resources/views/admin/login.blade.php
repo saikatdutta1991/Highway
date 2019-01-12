f29e827c09084bb8b07c5c330cced237d46b1f08
saikat <saikatdutta1991@gmail.com>
2019-01-12 18:20:49 +0530

    admin login page background video changed

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -48,7 +48,7 @@
     <body class="login-page">
         <video poster="{{url('admin_assets/Orchestra/Snapshots/Orchestra.jpg')}}" id="bgvid" playsinline autoplay muted loop>
             <!-- WCAG general accessibility recommendation is that media such as background video play through only once. Loop turned on for the purposes of illustration; if removed, the end of the video will fade in the same way created by pressing the "Pause" button  -->
-            <source src="{{url('admin_assets/Orchestra/WEBM/Orchestra.webm')}}" type="video/webm">
+            <!-- <source src="{{url('admin_assets/Orchestra/WEBM/Orchestra.webm')}}" type="video/webm"> -->
             <source src="{{url('admin_assets/Orchestra/Mp4/Orchestra.mp4')}}" type="video/mp4">
         </video>
         <div class="login-box">
