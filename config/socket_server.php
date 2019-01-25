285c85b89b0f624413076b926efb77a7d0f05125
saikat <saikatdutta1991@gmail.com>
2019-01-25 11:53:25 +0530

    end-sample updated and socket_server.config updated

--- config/socket_server.php
+++ config/socket_server.php
@@ -2,6 +2,6 @@
 
 //socket server configuration
 return [
-	"socket_url" => "http://localhost:3000",
-	"server_internal_communication_key" => "123456789"
+	"socket_url" => env('SOCKET_URL', 'https://highway.capefox.in:3000'),
+	"server_internal_communication_key" => env('SOCKET_COMM_KEY', '123456789')
 ];
