9ee4122bfb8d24032074559c79cc45d3e1910fee
Saikat Dutta <saikatdutta@Saikats-Air.Dlink>
2019-05-21 16:56:42 +0530

    loader updated admin panel

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -29,46 +29,142 @@
         <!-- <link href="{{url('admin_assets/admin_bsb')}}/css/themes/all-themes.css" rel="stylesheet" /> -->
         <link href="{{url('admin_assets/admin_bsb')}}/css/themes/theme-pink.min.css" rel="stylesheet" />
         <style>
-
             /* width */
             ::-webkit-scrollbar {
-                width: 5px;
+            width: 5px;
             }
-
             /* Track */
             ::-webkit-scrollbar-track {
-                background: #f1f1f1; 
+            background: #f1f1f1; 
             }
-
             /* Handle */
             ::-webkit-scrollbar-thumb {
-                background: #00baed; 
+            background: #00baed; 
             }
-
             /* Handle on hover */
             ::-webkit-scrollbar-thumb:hover {
-                background: #555; 
+            background: #555; 
             }
-
+            #loader-wrapper {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            z-index: 1000;
+            }
+            #loader {
+            display: block;
+            position: relative;
+            left: 50%;
+            top: 50%;
+            width: 150px;
+            height: 150px;
+            margin: -75px 0 0 -75px;
+            border-radius: 50%;
+            border: 3px solid transparent;
+            border-top-color: #3498db;
+            -webkit-animation: spin 2s linear infinite; /* Chrome, Opera 15+, Safari 5+ */
+            animation: spin 2s linear infinite; /* Chrome, Firefox 16+, IE 10+, Opera */
+            z-index: 1001;
+            }
+            #loader:before {
+            content: "";
+            position: absolute;
+            top: 5px;
+            left: 5px;
+            right: 5px;
+            bottom: 5px;
+            border-radius: 50%;
+            border: 3px solid transparent;
+            border-top-color: #e74c3c;
+            -webkit-animation: spin 3s linear infinite; /* Chrome, Opera 15+, Safari 5+ */
+            animation: spin 3s linear infinite; /* Chrome, Firefox 16+, IE 10+, Opera */
+            }
+            #loader:after {
+            content: "";
+            position: absolute;
+            top: 15px;
+            left: 15px;
+            right: 15px;
+            bottom: 15px;
+            border-radius: 50%;
+            border: 3px solid transparent;
+            border-top-color: #f9c922;
+            -webkit-animation: spin 1.5s linear infinite; /* Chrome, Opera 15+, Safari 5+ */
+            animation: spin 1.5s linear infinite; /* Chrome, Firefox 16+, IE 10+, Opera */
+            }
+            @-webkit-keyframes spin {
+            0%   { 
+            -webkit-transform: rotate(0deg);  /* Chrome, Opera 15+, Safari 3.1+ */  /* IE 9 */
+            transform: rotate(0deg);  /* Firefox 16+, IE 10+, Opera */
+            }
+            100% {
+            -webkit-transform: rotate(360deg);  /* Chrome, Opera 15+, Safari 3.1+ */  /* IE 9 */
+            transform: rotate(360deg);  /* Firefox 16+, IE 10+, Opera */
+            }
+            }
+            @keyframes spin {
+            0%   { 
+            -webkit-transform: rotate(0deg);  /* Chrome, Opera 15+, Safari 3.1+ */  /* IE 9 */
+            transform: rotate(0deg);  /* Firefox 16+, IE 10+, Opera */
+            }
+            100% {
+            -webkit-transform: rotate(360deg);  /* Chrome, Opera 15+, Safari 3.1+ */  /* IE 9 */
+            transform: rotate(360deg);  /* Firefox 16+, IE 10+, Opera */
+            }
+            }
+            #loader-wrapper .loader-section {
+            position: fixed;
+            top: 0;
+            width: 51%;
+            height: 100%;
+            background: #222222;
+            z-index: 1000;
+            -webkit-transform: translateX(0);  /* Chrome, Opera 15+, Safari 3.1+ */  /* IE 9 */
+            transform: translateX(0);  /* Firefox 16+, IE 10+, Opera */
+            }
+            #loader-wrapper .loader-section.section-left {
+            left: 0;
+            }
+            #loader-wrapper .loader-section.section-right {
+            right: 0;
+            }
+            /* Loaded */
+            .loaded #loader-wrapper .loader-section.section-left {
+            -webkit-transform: translateX(-100%);  /* Chrome, Opera 15+, Safari 3.1+ */  /* IE 9 */
+            transform: translateX(-100%);  /* Firefox 16+, IE 10+, Opera */
+            -webkit-transition: all 0.7s 0.3s cubic-bezier(0.645, 0.045, 0.355, 1.000);  
+            transition: all 0.7s 0.3s cubic-bezier(0.645, 0.045, 0.355, 1.000);
+            }
+            .loaded #loader-wrapper .loader-section.section-right {
+            -webkit-transform: translateX(100%);  /* Chrome, Opera 15+, Safari 3.1+ */  /* IE 9 */
+            transform: translateX(100%);  /* Firefox 16+, IE 10+, Opera */
+            -webkit-transition: all 0.7s 0.3s cubic-bezier(0.645, 0.045, 0.355, 1.000);  
+            transition: all 0.7s 0.3s cubic-bezier(0.645, 0.045, 0.355, 1.000);
+            }
+            .loaded #loader {
+            opacity: 0;
+            -webkit-transition: all 0.3s ease-out;  
+            transition: all 0.3s ease-out;
+            }
+            .loaded #loader-wrapper {
+            visibility: hidden;
+            -webkit-transform: translateY(-100%);  /* Chrome, Opera 15+, Safari 3.1+ */  /* IE 9 */
+            transform: translateY(-100%);  /* Firefox 16+, IE 10+, Opera */
+            -webkit-transition: all 0.3s 1s ease-out;  
+            transition: all 0.3s 1s ease-out;
+            }
         </style>
         @yield('top-header')
     </head>
     <body class="theme-pink">
         <!-- Page Loader -->
-        <div class="page-loader-wrapper">
-            <div class="loader">
-                <div class="preloader">
-                    <div class="spinner-layer pl-red">
-                        <div class="circle-clipper left">
-                            <div class="circle"></div>
-                        </div>
-                        <div class="circle-clipper right">
-                            <div class="circle"></div>
-                        </div>
-                    </div>
-                </div>
-                <p>Please wait...</p>
-            </div>
+        <div id="loader-wrapper">
+            <span id="loader-text">Initiating Blockchain...</span>
+            <div id="loader"></div>
+            <div class="loader-section section-right"></div>
+            <div class="loader-section section-left"></div>
         </div>
         <!-- #END# Page Loader -->
         <!-- Overlay For Sidebars -->
@@ -107,7 +203,6 @@
         <script src="{{url('admin_assets/admin_bsb')}}/plugins/morrisjs/morris.js"></script>
         <!-- ChartJs -->
         <script src="{{url('admin_assets/admin_bsb')}}/plugins/chartjs/Chart.bundle.js"></script>
-        
         <!-- Jquery Validation Plugin Css -->
         <script src="{{url('admin_assets/admin_bsb')}}/plugins/jquery-validation/jquery.validate.js"></script>
         <!-- Sparkline Chart Plugin Js -->
@@ -124,6 +219,25 @@
         <!-- SweetAlert Plugin Js -->
         <script src="{{url('admin_assets/admin_bsb')}}/plugins/sweetalert/sweetalert.min.js"></script>
         <!-- showNotification('bg-black', 'testing', 'top', 'right', 'animated flipInX', 'animated flipOutX'); -->
+        <script>
+            function hideLoader()
+            {
+                $("#loader").fadeOut()
+                setTimeout(() => {
+                    $(".section-left").animate({"left":"-1000px"}, "slow")
+                    $(".section-right").animate({"right":"-1000px"}, "slow")
+            
+                    setTimeout(() => {
+                        $("#loader-wrapper").remove()
+                    }, 500);
+            
+                }, 500);
+            }
+            
+            $(document).ready(()=>{
+                hideLoader();
+            })
+        </script>
         @yield('bottom')
     </body>
 </html>
