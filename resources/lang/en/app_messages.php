b74faf70553788082f5a97fb280028cb7bcb09b7
Saikat Dutta <saikatdutta@Saikats-Air.Dlink>
2019-06-01 13:26:17 +0530

    app_messages fixed

--- resources/lang/en/app_messages.php
+++ resources/lang/en/app_messages.php
@@ -6,6 +6,6 @@
 
 return [
     "otp_message" => "<#> Use OTP code :otp to verify your account on :appname. Keep this OTP to yourself for account safety. :apphash",
-    "booking_alert" => "Your upcoming trip starts on :date at :time. Please click the link to konw booking details and tack driver :tracklink and your boarding point :boardingpointlink",
+    "booking_alert" => "Your upcoming trip starts on :date at :time. Please click the link to konw booking details and track driver :tracklink and your boarding point :boardingpointlink",
     "booking_alert_push" => "Your upcoming booked trip starts on :date at :time"
 ];
