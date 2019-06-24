f121798d4cfa506b4a9eadd45a56037e9afe3e12
Saikat Dutta <saikatdutta@Saikats-Air.Dlink>
2019-06-24 11:54:31 +0530

    welcome page edit done

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -24,20 +24,17 @@
 <header class="bg-gradient" id="home">
     <div class="container mt-5">
         <h1>Welcome to <span class="website_name">{{$website_name}}</span>!</h1>
-        <p class="tagline" style="max-width: initial;">Enjoy hassle free rides within your own city or outside your city. <br>The ease of travel is on the finger tips, select the dates book your seats and hail towards your
-destination. There is so much more from just travelling to a comfort travel. <span class="website_name">{{$website_name}}</span> presents to you various options to travel. **terms and conditions apply**</p>
+        <p class="tagline" style="max-width: initial;">Enjoy hassle free rides within your own city or outside your city. 
+            <br>The ease of travel is on the finger tips, select the dates book your seats and hail towards your destination. There is so much more from just travelling to a comfort travel. 
+            <br><span class="website_name">{{$website_name}}</span> presents to you various options to travel. <a href="{{route('terms')}}" style="color: white;">**terms and conditions apply**</a>
+        </p>
     </div>
     <div class="img-holder mt-3"><img src="{{asset('images/phone_images/main_screen.png')}}" alt="phone" class="img-fluid"></div>
 </header>
-<!-- <div class="client-logos my-5">
-    <div class="container text-center">
-        <img src="{{asset('web/home/')}}/images/client-logos.png" alt="client logos" class="img-fluid">
-    </div>
-</div> -->
 <div class="section light-bg" id="features">
     <div class="container">
         <div class="section-title">
-            <small>HIGHLIGHTS</small>
+            <!-- <small>HIGHLIGHTS</small> -->
             <h3>Services you love</h3>
         </div>
         <div class="row">
@@ -45,9 +42,9 @@ destination. There is so much more from just travelling to a comfort travel. <sp
                 <div class="card features">
                     <div class="card-body">
                         <div class="media">
-                            <span class="ti-face-smile gradient-fill ti-3x mr-3"></span>
+                            <!-- <span class="ti-face-smile gradient-fill ti-3x mr-3"></span> -->
                             <div class="media-body">
-                                <h4 class="card-title">City Ride</h4>
+                                <h4 class="card-title">City ride</h4>
                                 <p class="card-text">Quick and easy rides within your city, choose from options like auto to SUVs depending on the number of people travelling and move on the go!</p>
                             </div>
                         </div>
@@ -58,9 +55,9 @@ destination. There is so much more from just travelling to a comfort travel. <sp
                 <div class="card features">
                     <div class="card-body">
                         <div class="media">
-                            <span class="ti-settings gradient-fill ti-3x mr-3"></span>
+                            <!-- <span class="ti-settings gradient-fill ti-3x mr-3"></span> -->
                             <div class="media-body">
-                                <h4 class="card-title">Highway Ride</h4>
+                                <h4 class="card-title">Highway ride</h4>
                                 <p class="card-text">Easy travel like never before, schedule your trips few hours ago for out station ride from the available destination, and once confirmed fascinate your travel world or work commitments with a comfort travel in a cab rather than waiting for other modes of transport.</p>
                             </div>
                         </div>
@@ -71,7 +68,7 @@ destination. There is so much more from just travelling to a comfort travel. <sp
                 <div class="card features">
                     <div class="card-body">
                         <div class="media">
-                            <span class="ti-lock gradient-fill ti-3x mr-3"></span>
+                            <!-- <span class="ti-lock gradient-fill ti-3x mr-3"></span> -->
                             <div class="media-body">
                                 <h4 class="card-title">Simple</h4>
                                 <p class="card-text">The app is user friendly and easy to use. Click on the sign up option, complete the KYC and there you land on the booking page with self explanatory options.</p>
@@ -84,7 +81,7 @@ destination. There is so much more from just travelling to a comfort travel. <sp
                 <div class="card features">
                     <div class="card-body">
                         <div class="media">
-                            <span class="ti-lock gradient-fill ti-3x mr-3"></span>
+                            <!-- <span class="ti-lock gradient-fill ti-3x mr-3"></span> -->
                             <div class="media-body">
                                 <h4 class="card-title">Secure</h4>
                                 <p class="card-text">All the user data is secured and is as per the privacy policy.</p>
@@ -96,28 +93,23 @@ destination. There is so much more from just travelling to a comfort travel. <sp
         </div>
     </div>
 </div>
-<!-- // end .section -->
-{{--<div class="section">
+<div class="section light-bg">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-6 offset-lg-6">
-                <!-- <div class="box-icon"><span class="ti-mobile gradient-fill ti-3x"></span></div> -->
-                <h2>Our mission statement</h2>
-                <p class="mb-4">Our priorities are our customers. We would want to help customers succeed in there travel goals with the available transport options with us. Customers comfort and there choices matter to us. Hence, we believe in delivering quality services with customers as our primary source of facilitators.</p>
-                <a href="javascript:void(0)" class="btn btn-primary">Let India travel!!!</a>
-            </div>
+        <div class="section-title">
+            <small>FEATURES</small>
+            <h3>Discover our appp</h3>
         </div>
-        <div class="perspective-phone">
-            <img src="{{asset('web/home/')}}/images/perspective.png" alt="perspective phone" class="img-fluid">
+        <div class="container text-center">
+            <iframe width="966" height="543" src="https://www.youtube.com/embed/5Peo-ivmupE?autoplay=1&&controls=0&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     </div>
-</div>--}}
+</div>
 <!-- // end .section -->
 <div class="section light-bg">
     <div class="container">
         <div class="section-title">
             <small>FEATURES</small>
-            <h3>Do more with our app</h3>
+            <h3>Discover our appp</h3>
         </div>
         <ul class="nav nav-tabs nav-justified" role="tablist">
             <li class="nav-item">
@@ -126,70 +118,22 @@ destination. There is so much more from just travelling to a comfort travel. <sp
             <li class="nav-item">
                 <a class="nav-link" data-toggle="tab" href="#schedule">Scheduling</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" data-toggle="tab" href="#messages">Messages</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-toggle="tab" href="#livechat">Live Chat</a>
-            </li>
         </ul>
         <div class="tab-content">
             <div class="tab-pane fade show active" id="communication">
                 <div class="d-flex flex-column flex-lg-row">
                     <img src="{{asset('web/home/')}}/images/graphic.png" alt="graphic" class="img-fluid rounded align-self-start mr-lg-5 mb-5 mb-lg-0">
                     <div>
-                        <h2>Communicate with ease</h2>
-                        <p class="lead">Uniquely underwhelm premium outsourcing with proactive leadership skills. </p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rutrum, urna eu pellentesque pretium, nisi nisi fermentum enim, et sagittis dolor nulla vel sapien. Vestibulum sit amet mattis ante. Ut placerat dui eu nulla
-                            congue tincidunt ac a nibh. Mauris accumsan pulvinar lorem placerat volutpat. Praesent quis facilisis elit. Sed condimentum neque quis ex porttitor,
-                        </p>
-                        <p> malesuada faucibus augue aliquet. Sed elit est, eleifend sed dapibus a, semper a eros. Vestibulum blandit vulputate pharetra. Phasellus lobortis leo a nisl euismod, eu faucibus justo sollicitudin. Mauris consectetur, tortor
-                            sed tempor malesuada, sem nunc porta augue, in dictum arcu tortor id turpis. Proin aliquet vulputate aliquam.
-                        </p>
+                        <!-- <h2>Communicate with ease</h2> -->
+                        <!-- <p class="lead">Uniquely underwhelm premium outsourcing with proactive leadership skills. </p> -->
+                        <p>We at <span id="website_name_navbar">{{$website_name}}</span> , believes in keeping the rider/partner informed about there upcoming rides or send them notifications to keep them on the tap of what is happening with their ride. Communicate with ease. Just email us on <span style="color:black">{{$website_contact_email}}</span> if you have any query. We are ready to help you with the best solutions.</p>
                     </div>
                 </div>
             </div>
             <div class="tab-pane fade" id="schedule">
                 <div class="d-flex flex-column flex-lg-row">
                     <div>
-                        <h2>Scheduling when you want</h2>
-                        <p class="lead">Uniquely underwhelm premium outsourcing with proactive leadership skills. </p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rutrum, urna eu pellentesque pretium, nisi nisi fermentum enim, et sagittis dolor nulla vel sapien. Vestibulum sit amet mattis ante. Ut placerat dui eu nulla
-                            congue tincidunt ac a nibh. Mauris accumsan pulvinar lorem placerat volutpat. Praesent quis facilisis elit. Sed condimentum neque quis ex porttitor,
-                        </p>
-                        <p> malesuada faucibus augue aliquet. Sed elit est, eleifend sed dapibus a, semper a eros. Vestibulum blandit vulputate pharetra. Phasellus lobortis leo a nisl euismod, eu faucibus justo sollicitudin. Mauris consectetur, tortor
-                            sed tempor malesuada, sem nunc porta augue, in dictum arcu tortor id turpis. Proin aliquet vulputate aliquam.
-                        </p>
-                    </div>
-                    <img src="{{asset('web/home/')}}/images/graphic.png" alt="graphic" class="img-fluid rounded align-self-start mr-lg-5 mb-5 mb-lg-0">
-                </div>
-            </div>
-            <div class="tab-pane fade" id="messages">
-                <div class="d-flex flex-column flex-lg-row">
-                    <img src="{{asset('web/home/')}}/images/graphic.png" alt="graphic" class="img-fluid rounded align-self-start mr-lg-5 mb-5 mb-lg-0">
-                    <div>
-                        <h2>Realtime Messaging service</h2>
-                        <p class="lead">Uniquely underwhelm premium outsourcing with proactive leadership skills. </p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rutrum, urna eu pellentesque pretium, nisi nisi fermentum enim, et sagittis dolor nulla vel sapien. Vestibulum sit amet mattis ante. Ut placerat dui eu nulla
-                            congue tincidunt ac a nibh. Mauris accumsan pulvinar lorem placerat volutpat. Praesent quis facilisis elit. Sed condimentum neque quis ex porttitor,
-                        </p>
-                        <p> malesuada faucibus augue aliquet. Sed elit est, eleifend sed dapibus a, semper a eros. Vestibulum blandit vulputate pharetra. Phasellus lobortis leo a nisl euismod, eu faucibus justo sollicitudin. Mauris consectetur, tortor
-                            sed tempor malesuada, sem nunc porta augue, in dictum arcu tortor id turpis. Proin aliquet vulputate aliquam.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="tab-pane fade" id="livechat">
-                <div class="d-flex flex-column flex-lg-row">
-                    <div>
-                        <h2>Live chat when you needed</h2>
-                        <p class="lead">Uniquely underwhelm premium outsourcing with proactive leadership skills. </p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rutrum, urna eu pellentesque pretium, nisi nisi fermentum enim, et sagittis dolor nulla vel sapien. Vestibulum sit amet mattis ante. Ut placerat dui eu nulla
-                            congue tincidunt ac a nibh. Mauris accumsan pulvinar lorem placerat volutpat. Praesent quis facilisis elit. Sed condimentum neque quis ex porttitor,
-                        </p>
-                        <p> malesuada faucibus augue aliquet. Sed elit est, eleifend sed dapibus a, semper a eros. Vestibulum blandit vulputate pharetra. Phasellus lobortis leo a nisl euismod, eu faucibus justo sollicitudin. Mauris consectetur, tortor
-                            sed tempor malesuada, sem nunc porta augue, in dictum arcu tortor id turpis. Proin aliquet vulputate aliquam.
-                        </p>
+                        <p>Schedule ride as per your convenience, by searching for available schedules on a given day. Search and book a ride for outstation within few hours* and then relax doing your ongoing work.</p>
                     </div>
                     <img src="{{asset('web/home/')}}/images/graphic.png" alt="graphic" class="img-fluid rounded align-self-start mr-lg-5 mb-5 mb-lg-0">
                 </div>
@@ -224,21 +168,21 @@ destination. There is so much more from just travelling to a comfort travel. <sp
                         <div class="circle-icon mr-4">1</div>
                         <div class="media-body">
                             <h5>Create an Account</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rutrum, urna eu pellentesque pretium obcaecati vel exercitationem </p>
+                            <p>Sign up with the available options and fill the required details. One single click with filled information and there you are to use the byroad services.</p>
                         </div>
                     </li>
                     <li class="media my-4">
                         <div class="circle-icon mr-4">2</div>
                         <div class="media-body">
                             <h5>Share with friends</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rutrum, urna eu pellentesque pretium obcaecati vel exercitationem eveniet</p>
+                            <p>What more you could have asked. Now enjoy and avail good offers when you share the byroad app with your friends. Enjoy the referral bonus and make your ride a successful trip.</p>
                         </div>
                     </li>
                     <li class="media">
                         <div class="circle-icon mr-4">3</div>
                         <div class="media-body">
                             <h5>Enjoy your life</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rutrum, urna eu pellentesque pretium obcaecati vel exercitationem </p>
+                            <p>Not only can you go for a ride alone, you can also book seats for your friends and family to travel together. Enjoy this beautiful journey called life with comfortable travel options.</p>
                         </div>
                     </li>
                 </ul>
