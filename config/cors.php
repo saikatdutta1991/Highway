7eebef729e66d8944287970573d9204c2f229eac
Saikat Dutta <saikatdutta@Saikats-Air.Dlink>
2019-06-26 20:19:48 +0530

    asad

--- config/cors.php
+++ config/cors.php
@@ -15,9 +15,9 @@ return [
     'supportsCredentials' => true,
     'allowedOrigins' => ['*'],
     'allowedOriginsPatterns' => [],
-    'allowedHeaders' => ['Origin, Content-Type, Accept, Authorization, X-Request-With, cache-control,postman-token, token'],
+    'allowedHeaders' => ['Origin', 'Content-Type', 'Accept', 'Authorization', 'X-Request-With', 'cache-control', 'postman-token', 'token'],
     'allowedMethods' => ['*'],
-    'exposedHeaders' => ['Origin, Content-Type, Accept, Authorization, X-Request-With, cache-control,postman-token, token'],
+    'exposedHeaders' => [],
     'maxAge' => 0,
 
 ];
