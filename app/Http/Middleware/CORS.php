a1d99c32417f97f92f5fb76d7f60f0338bfb802b
ʇɐʞıɐs ɐʇʇnp <saikatdutta1991@users.noreply.github.com>
2019-06-26 14:18:02 +0530

    Update CORS.php

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -15,7 +15,7 @@ class CORS
      */
     public function handle($request, Closure $next)
     {
-       
+       dd('cors');
         $headers = [
             'Access-Control-Allow-Origin' => '*',
             'Access-Control-Allow-Methods' => 'Allow, POST, GET, OPTIONS, PUT, DELETE',
